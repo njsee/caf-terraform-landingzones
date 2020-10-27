7dc009db8b5230d5a5a92983709c605e847d4db0
NJSee <48836977+njsee@users.noreply.github.com>
2020-10-26 22:44:32 -0700

    updated maintf

--- landingzones/landingzone_caf_foundations/main.tf
+++ landingzones/landingzone_caf_foundations/main.tf
@@ -33,15 +33,15 @@ locals {
   tags = merge(var.tags, local.landingzone_tag)
 }
 
-data "terraform_remote_state" "launchpad" {
-  backend = "azurerm"
-  config = {
-    storage_account_name = var.lowerlevel_storage_account_name
-    container_name       = var.lowerlevel_container_name
-    key                  = var.lowerlevel_key
-    resource_group_name  = var.lowerlevel_resource_group_name
-  }
-}
+#data "terraform_remote_state" "launchpad" {
+#  backend = "azurerm"
+ # config = {
+#    storage_account_name = var.lowerlevel_storage_account_name
+#    container_name       = var.lowerlevel_container_name
+#    key                  = var.lowerlevel_key
+#    resource_group_name  = var.lowerlevel_resource_group_name
+#  }
+#}
 
 locals {
   global_settings     = data.terraform_remote_state.launchpad.outputs.global_settings
