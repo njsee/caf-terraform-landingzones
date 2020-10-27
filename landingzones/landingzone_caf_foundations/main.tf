c5d410478162a1cfef98bb956402e73af9a91df0
NJSee <48836977+njsee@users.noreply.github.com>
2020-10-26 22:46:13 -0700

    update main

--- landingzones/landingzone_caf_foundations/main.tf
+++ landingzones/landingzone_caf_foundations/main.tf
@@ -44,9 +44,9 @@ locals {
 #}
 
 locals {
-  global_settings     = data.terraform_remote_state.launchpad.outputs.global_settings
+ # global_settings     = data.terraform_remote_state.launchpad.outputs.global_settings
   prefix              = var.prefix == null ? local.global_settings.prefix : var.prefix
   environment         = local.global_settings.environment
   tags_hub            = merge({ "environment" = local.environment }, var.global_settings.tags_hub)
-  azure_subscriptions = data.terraform_remote_state.launchpad.outputs.azure_subscriptions
+ # azure_subscriptions = data.terraform_remote_state.launchpad.outputs.azure_subscriptions
 }
